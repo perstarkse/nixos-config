78ecb66aca930584b5ba2160131df1c5404b19dd
Per Stark <perstark.se@gmail.com>
2023-07-28 20:56:45 +0200

    old i3status conf

--- programs/i3status-rust/default.nix
+++ programs/i3status-rust/default.nix
@@ -1,47 +1,67 @@
-{config, pkgs, lib, ...}:
+{ config, lib, pkgs, ... }:
+
 let
-  hostName = pkgs.lib.head (pkgs.lib.splitString "\n" (pkgs.runCommandLocal "getHostname" {} "uname -n"));
-in
-{
-  programs.i3status-rust = {
-    enable = true;
-    bars = {
-      bottom = {
-        theme = "dracula";
-        icons = "awesome6";
-        blocks = [
-        {
-          alert = 10.0;
-          block = "disk_space";
-          info_type = "available";
-          interval = 60;
-          path = "/";
-          warning = 20.0;
-        }
-        {
-          block = "custom";
-          command = "cat /etc/hostname";
-          interval = "once";
-        }
-        {
-          block = "time";
-          format = " $timestamp.datetime(f:'%a %d/%m %R') ";
-          interval = 60;
-        }
-        {
-          block = "sound";
-        }
-        {
-          block = "custom";
-          command = ''
-          nmcli con show --active | grep wireguard | awk '{print $1 }'
-          '';
-          interval = 10;
-        }
-        ] ++ lib.optional (hostName == "ariel") {
-          block = "battery";
-          };
+  commonBlocks = [
+    {
+      alert = 10.0;
+      block = "disk_space";
+      info_type = "available";
+      interval = 60;
+      path = "/";
+      warning = 20.0;
+    }
+    {
+      block = "custom";
+      command = "cat /etc/hostname";
+      interval = "once";
+    }
+    {
+      block = "time";
+      format = " $timestamp.datetime(f:'%a %d/%m %R') ";
+      interval = 60;
+    }
+    {
+      block = "sound";
+    }
+    {
+      block = "custom";
+      command = ''
+      nmcli con show --active | grep wireguard | awk '{print $1 }'
+      '';
+      interval = 10;
+    }
+  ];
+
+  hostname = builtins.readFile "/etc/hostname";
+
+  i3statusConfig = if hostname == "ariel" then
+    {
+      enable = true;
+      bars = {
+        bottom = {
+          theme = "dracula";
+          icons = "awesome6";
+          blocks = commonBlocks ++ [
+            {
+              block = "battery";
+              interval = 10;
+            }
+          ];
+        };
+      };
+    }
+  else
+    {
+      enable = true;
+      bars = {
+        bottom = {
+          theme = "dracula";
+          icons = "awesome6";
+          blocks = commonBlocks;
         };
       };
     };
+in
+{
+  programs.i3status-rust = i3statusConfig;
 }
