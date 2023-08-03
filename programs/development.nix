dbe7bb5b948307f619b8a7aa5efa2a436338cdd9
Per Stark <perstark.se@gmail.com>
2023-08-03 08:52:45 +0200

    readding development.nix

---
+++ programs/development.nix
@@ -0,0 +1,20 @@
+{pkgs, ... }:
+{
+  imports = [
+    ./vscode
+  ]; 
+  
+  home.packages = with pkgs; [ 
+    aws-sso-cli
+    nodejs
+    
+    # lsps
+    nodePackages.typescript-language-server
+    python311Packages.python-lsp-server
+    rust-analyzer
+    nodePackages.vscode-css-languageserver-bin
+    marksman
+    nil
+    nodePackages.bash-language-server     
+  ];
+}
