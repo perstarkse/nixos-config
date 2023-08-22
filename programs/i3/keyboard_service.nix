342bcddf137eee1829a460765973f6f087f2ac10
Per Stark <perstark.se@gmail.com>
2023-08-22 11:38:58 +0200

    chore: Remove redundant sleep command in changeKeyboardLayoutScript
    - Remove redundant `sleep 5` command from `changeKeyboardLayoutScript`
    - Update `flake.lock` file

--- programs/i3/keyboard_service.nix
+++ programs/i3/keyboard_service.nix
@@ -30,23 +30,22 @@ with lib;
     changeKeyboardLayoutScript = pkgs.writeScript "keyboard-layout-switcher.sh" ''
     #!${pkgs.bash}/bin/bash
 
-    sleep 5
-    
     ${pkgs.i3}/bin/i3-msg -t subscribe -m '[ "window", "focus" ]' | \
     ${pkgs.jq}/bin/jq --unbuffered -r 'select(.change == "focus").container.window_properties.instance' |
-    while read -r window_class; do
-      if echo '${builtins.concatStringsSep " " cfg.optionalLayoutPrograms}' | ${pkgs.gnugrep}/bin/grep -q "$window_class"; then
+      while read -r window_class; do
+        if echo '${builtins.concatStringsSep " " cfg.optionalLayoutPrograms}' | ${pkgs.gnugrep}/bin/grep -q "$window_class"; then
             ${pkgs.xorg.setxkbmap}/bin/setxkbmap ${cfg.optionalLayout}
         else
             ${pkgs.xorg.setxkbmap}/bin/setxkbmap ${cfg.defaultLayout}
         fi
-     done
-  '';
-   in
-   mkIf cfg.enable {
+       done
+    '';
+  in
+  mkIf cfg.enable {
     systemd.user.services.keyboard-layout-switcher = {
     Unit = {
       Description = "Change keyboard layout based on focused i3 window";
+      After = [ "graphical.target" ];
     };
     Install = {
       WantedBy = [ "default.target" ];
